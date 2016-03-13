5546046d2e44b7e2ab3038e14d8075a3832ea9e6
Daniel Vu <daniel.h.vu@gmail.com>
2016-03-12 20:39:05 -0600

    Reimplemented date of tweets by using jquery.timeago.js library. Changed theme of site to look similiar to twitter. Fixed bug of tweets being deleted after clicking a users twitter handle.

--- javascript/twittler.js
+++ javascript/twittler.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+      showTweets();
+
       $("body").on("click", "a", function(){
         var userName = $(this).text().substring(1);
         showTweets(userName);
@@ -9,40 +11,10 @@ $(document).ready(function(){
         showTweets();
       });
       
-      function formatTimestamp(tweetCreationTime){
-        var year = tweetCreationTime.getFullYear();
-        var month = formatMonth(tweetCreationTime.getMonth());
-        var day = tweetCreationTime.getDay();
-        var hour = tweetCreationTime.getHours();
-        var mintue = tweetCreationTime.getMinutes();
-        var second = tweetCreationTime.getSeconds();
-        if (hour > 12 ){
-          hour -= 12;
-          second += " PM";
-        }
-        else{
-          second += " AM";
-        }
-        var formatedDate = month + " " + day + ", " + year + " " + hour + ":" + mintue + ":" + second;
-        return formatedDate;
-      }
-
-      function formatMonth(monthIndex){
-        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        return months[monthIndex];
-      }
-
-      // function showUserTimeline(userName){
-      //   var usersNameTweets = streams.users[userName];
-      //   for (var i = 0; i < usersNameTweets.length i++){
-          
-      //   }
-      // }
-
       function showTweets(userName) {
           if (arguments.length < 1) {
             $('.tweets').empty();
-            var homeTweetsLength = streams.home.length-1;
+            var homeTweetsLength = (streams.home.length)-1;
             for (var i = homeTweetsLength; i > 0; i--) {
                 var tweet = streams.home[i];
                 var tweetTag = $('<a></a>');
@@ -50,9 +22,10 @@ $(document).ready(function(){
                 var tweetMessage = $('<span></span>');
 
                 tweetTag.text('@' + tweet.user);
-                tweetDate.text(formatTimestamp(tweet.created_at));
+                tweetDate.text("- " + jQuery.timeago(tweet.created_at));
                 tweetMessage.text(tweet.message);
 
+                
                 tweetTag.appendTo($('.tweets'));
                 tweetDate.appendTo($('.tweets'));
                 tweetMessage.appendTo($('.tweets'));
@@ -60,15 +33,15 @@ $(document).ready(function(){
           }
           else{
             $('.tweets').empty();
-            var userTweetsLength = streams.users[userName].length-1;
+            var userTweetsLength = (streams.users[userName].length)-1;
             for (var i = userTweetsLength; i > 0; i--){
                 var tweet = streams.users[userName][i];
                 var tweetTag = $('<a></a>');
                 var tweetDate = $('<p><p');
                 var tweetMessage = $('<span></span>');
 
-                tweetTag.text('@' + userName + " - ");
-                tweetDate.text("tweeted on " + formatTimestamp(tweet.created_at));
+                tweetTag.text('@' + userName);
+                tweetDate.text("- " + jQuery.timeago(tweet.created_at));
                 tweetMessage.text(tweet.message);
 
                 tweetTag.appendTo($('.tweets'));
@@ -78,43 +51,7 @@ $(document).ready(function(){
 
           }
       }
-      showTweets();
       
-
-      // }
-
-
-
-      // var index = streams.home.length - 1;
-      // while(index >= 0){
-      //   // var tweet = streams.home[index];
-      //   // var $tweet = $('<div></div>');
-      //   // $tweet.text('@' + tweet.user + ': ' + tweet.message + ' - ' + formatTimestamp(tweet.created_at) );
-      //   // $tweet.appendTo($body);
-      //   // index -= 1;
-
-      //   // var tweet = streams.home[index];
-      //   // var user = $('<div></div>');
-      //   // user.text('@' + tweet.user);
-      //   // user.appendTo($('listOfTweets'));
-      //   // index -= 1;
-
-      //   var tweet = streams.home[index];
-      //   var tweetTag = $('<a></a>');
-      //   var tweetDate = $('<p><p');
-      //   var tweetMessage = $('<span></span>');
-
-      //   tweetTag.text('@' + tweet.user);
-      //   tweetDate.text(formatTimestamp(tweet.created_at));
-      //   tweetMessage.text(tweet.message);
-
-      //   tweetTag.appendTo($('.tweets'));
-      //   tweetDate.appendTo($('.tweets'));
-      //   tweetMessage.appendTo($('.tweets'));
-
-      //   index -= 1;
-      // }
-
       for(var i = 0; i < window.users.length; i++){
         $('<a></a>').text('@' + users[i]).appendTo($('.followers'));
       }
